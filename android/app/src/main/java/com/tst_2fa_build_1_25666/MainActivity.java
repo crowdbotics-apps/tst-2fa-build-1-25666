21402500dd06ea9e745c56b02971d70edbc47e1a
Crowdbotics <team@crowdbotics.com>
2021-04-13 18:03:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tst_2fa_build_1_25666/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tst_2fa_build_1_25666;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tst_2fa_build_1_25666";
+  }
+}
